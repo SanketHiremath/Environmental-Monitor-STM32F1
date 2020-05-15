3b6082ccdae838a199283ededc422b93a8835961
SanketHiremath <sanketh63@gmail.com>
2020-05-15 18:33:36 +0530

    Adding the code

---
+++ Core/Src/main.c
@@ -0,0 +1,729 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "ssd1306.h"
+#include "fonts.h"
+#include "test.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "math.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#define thermal_address 0x45
+#define co2sensor_address 0x58
+#define lux_address 0x44
+#define DS3231_ADDRESS 0xD0
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+uint8_t data[2];
+uint8_t light_databuff[3];
+uint16_t co2sensor_init;
+unsigned char readbuff[7];
+int16_t temp_shift[9];
+int16_t temp_temp[9];
+unsigned char readbuff_2[19];
+int16_t temp_shift_2[9];
+int16_t temp_temp_2[9];
+int temp;
+int humi;
+float cHumi;
+float cTemp;
+float co2;
+float tvoc;
+unsigned int rawlux;
+float flux;
+float i;
+unsigned int ledcount=0;
+unsigned int displaymode=0;
+//uint8_t cite;cihu;cico;citv;cito;
+uint8_t sam[1];int bpflag=0;char char_buff1[6];char char_buff2[6];float T;
+char hrtime[],mintime[],sectime[];
+unsigned int workflag=0;
+
+
+
+uint32_t noise_val;
+int dbb=0;
+int diff;
+uint16_t mode=0;
+int s=0;
+int startp=59;
+int	prevx=16;
+int prevy=58;
+float xpos=14;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+void init_co2();
+void get_co2_voc();
+void get_temp_humi();
+void calculate_lux();
+void get_lux();
+void init_LightSensor();
+void DrawGraph(uint8_t i,uint8_t y);
+void DrawTempGraph(float i);
+float mapf(float val, float in_min, float in_max, float out_min, float out_max);
+//void set_rgb(uint8_t red,uint8_t green,uint8_t blue);
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+I2C_HandleTypeDef hi2c1;
+
+RTC_HandleTypeDef hrtc;
+RTC_TimeTypeDef sTime;
+RTC_TimeTypeDef sDate;
+TIM_HandleTypeDef htim3;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+//RTC_TimeTypeDef sTime;
+//RTC_DateTypeDef DateToUpdate;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_RTC_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+uint8_t decToBcd(int val)
+{
+  return (uint8_t)( (val/10*16) + (val%10) );
+}
+// Convert binary coded decimal to normal decimal numbers
+int bcdToDec(uint8_t val)
+{
+  return (int)( (val/16*10) + (val%16) );
+}
+
+typedef struct {
+	uint8_t seconds;
+	uint8_t minutes;
+	uint8_t hour;
+	uint8_t dayofweek;
+	uint8_t dayofmonth;
+	uint8_t month;
+	uint8_t year;
+} TIME;
+
+TIME time;
+
+// function to set time
+
+void Set_Time (uint8_t sec, uint8_t min, uint8_t hour, uint8_t dow, uint8_t dom, uint8_t month, uint8_t year)
+{
+	uint8_t set_time[7];
+	set_time[0] = decToBcd(sec);
+	set_time[1] = decToBcd(min);
+	set_time[2] = decToBcd(hour);
+	set_time[3] = decToBcd(dow);
+	set_time[4] = decToBcd(dom);
+	set_time[5] = decToBcd(month);
+	set_time[6] = decToBcd(year);
+
+	HAL_I2C_Mem_Write(&hi2c1, DS3231_ADDRESS, 0x00, 1, set_time, 7, 1000);
+}
+
+void Get_Time (void)
+{
+	uint8_t get_time[7];
+	HAL_I2C_Mem_Read(&hi2c1, DS3231_ADDRESS, 0x00, 1, get_time, 7, 1000);
+	time.seconds = bcdToDec(get_time[0]);
+	time.minutes = bcdToDec(get_time[1]);
+	time.hour = bcdToDec(get_time[2]);
+	time.dayofweek = bcdToDec(get_time[3]);
+	time.dayofmonth = bcdToDec(get_time[4]);
+	time.month = bcdToDec(get_time[5]);
+	time.year = bcdToDec(get_time[6]);
+}
+
+void pir(void)
+{
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_1,GPIO_PIN_RESET);
+		HAL_TIM_Base_Start_IT(&htim3);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_ADC1_Init();
+  MX_USART1_UART_Init();
+//  MX_RTC_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+
+//    HAL_RTC_Init(&hrtc);
+//    HAL_GPIO_WritePin(GPIOC,GPIO_PIN_13, GPIO_PIN_RESET);
+    init_co2();
+    init_LightSensor();
+    SSD1306_Init();
+    SSD1306_Fill(0);
+    SSD1306_GotoXY(0,0);
+    SSD1306_Puts("Temp:", &Font_7x10, SSD1306_COLOR_WHITE);
+    SSD1306_GotoXY(0,12);
+    SSD1306_Puts("Humi:", &Font_7x10, SSD1306_COLOR_WHITE);
+    SSD1306_UpdateScreen();
+    SSD1306_GotoXY(0,22);
+    SSD1306_Puts("CO2:", &Font_7x10, SSD1306_COLOR_WHITE);
+    SSD1306_UpdateScreen();
+    SSD1306_GotoXY(0,32);
+    SSD1306_Puts("VOC:", &Font_7x10, SSD1306_COLOR_WHITE);
+    SSD1306_UpdateScreen();
+    SSD1306_GotoXY(0,42);
+    SSD1306_Puts("Light:", &Font_7x10, SSD1306_COLOR_WHITE);
+    SSD1306_UpdateScreen();
+    SSD1306_GotoXY(0,52);
+    SSD1306_Puts("Time:", &Font_7x10, SSD1306_COLOR_WHITE);
+    SSD1306_UpdateScreen();
+  /* USER CODE END 2 */
+ 
+ 
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+	  ///////////////////////////////////////////////////////////////////////////////////temp and humidity
+	    get_temp_humi();
+		SSD1306_GotoXY(42,0);
+		sprintf(char_buff1,"%.2f",cTemp);
+		SSD1306_Puts(char_buff1, &Font_7x10, SSD1306_COLOR_WHITE);
+		SSD1306_UpdateScreen();
+		HAL_Delay(1000);
+
+		 SSD1306_GotoXY(42,12);
+		 sprintf(char_buff2,"%.2f",cHumi);
+		SSD1306_Puts(char_buff2, &Font_7x10, SSD1306_COLOR_WHITE);
+	    SSD1306_UpdateScreen();
+//      /////////////////////////////////////////////////////////////////////////////////co2 and tvoc
+	    get_co2_voc();
+		 SSD1306_GotoXY(42,22);
+		 sprintf(char_buff1,"%.2f",co2);
+		SSD1306_Puts(char_buff1, &Font_7x10, SSD1306_COLOR_WHITE);
+	    SSD1306_UpdateScreen();
+
+		 SSD1306_GotoXY(42,32);
+		 sprintf(char_buff2,"%.2f",tvoc);
+		SSD1306_Puts(char_buff2, &Font_7x10, SSD1306_COLOR_WHITE);
+	    SSD1306_UpdateScreen();
+//	 ///////////////////////////////////////////////////////////////////////////////////Ambient light
+		  get_lux();
+		  calculate_lux();
+		  SSD1306_GotoXY(42,42);
+		  sprintf(char_buff2,"%.1f",flux);
+		  SSD1306_Puts(char_buff2, &Font_7x10, SSD1306_COLOR_WHITE);
+		  SSD1306_UpdateScreen();
+    //////////////////////////////////////////////////////////////////////////////////////Time
+
+		  Get_Time();
+		  SSD1306_DrawFilledRectangle(42, 52, 86, 12, SSD1306_COLOR_BLACK);  //to clear the previous time displayed
+		  SSD1306_UpdateScreen();
+		  SSD1306_GotoXY(42,52);
+		  sprintf(hrtime,"%u",time.hour);
+		  SSD1306_Puts(hrtime, &Font_7x10, SSD1306_COLOR_WHITE);
+		  SSD1306_GotoXY(57,52);
+		  SSD1306_Puts(":", &Font_7x10, SSD1306_COLOR_WHITE);
+		  SSD1306_GotoXY(64,52);
+		  sprintf(mintime,"%u",time.minutes);
+		  SSD1306_Puts(mintime, &Font_7x10, SSD1306_COLOR_WHITE);
+		  SSD1306_GotoXY(79,52);
+		  SSD1306_Puts(":", &Font_7x10, SSD1306_COLOR_WHITE);
+		  SSD1306_GotoXY(86,52);
+		  sprintf(sectime,"%u",time.seconds);
+		  SSD1306_Puts(sectime, &Font_7x10, SSD1306_COLOR_WHITE);
+		  SSD1306_UpdateScreen();
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+
+void init_LightSensor()
+{
+	  light_databuff[0]=0x01;
+	  light_databuff[1]=(0xCE10)>>8;
+	  light_databuff[2]=(0xCE10)& 0x00FF;
+	  HAL_I2C_Master_Transmit(&hi2c1,lux_address<<1,light_databuff,3,50);
+}
+
+void get_lux()
+{
+	data[0]=(0x00);
+	HAL_I2C_Master_Transmit(&hi2c1,lux_address<<1,data,1,50);
+	HAL_Delay(50);
+	HAL_I2C_Master_Receive(&hi2c1,lux_address<<1,readbuff,2,50);
+	rawlux=((readbuff[0] << 8) | readbuff[1]);
+}
+
+void calculate_lux()
+{
+	 uint16_t iExponent, iMantissa;
+	  iMantissa = rawlux & 0x0FFF;
+	  iExponent = (rawlux & 0xF000) >> 12;
+	  flux= iMantissa * (0.01 * powf(2, iExponent));
+}
+
+void init_co2()
+{
+data[0]=(0x2003)>>8;
+data[1]=(0x2003) & 0x00FF;
+HAL_I2C_Master_Transmit(&hi2c1,co2sensor_address<<1,data,2,50);
+}
+
+void get_co2_voc()
+{
+	data[0]=(0x2008)>>8;
+	data[1]=(0x2008) & 0x00FF;
+	co2sensor_init=0x2008;
+	HAL_I2C_Master_Transmit(&hi2c1,co2sensor_address<<1,data,2,50);
+	HAL_Delay(1000);
+	HAL_I2C_Master_Receive(&hi2c1,co2sensor_address<<1,readbuff,6,50);
+	co2 = (readbuff[0]<<8)|readbuff[1];
+	tvoc=(readbuff[3]<<8)|(readbuff[4]);
+}
+
+void get_temp_humi()
+{
+	HAL_Delay(50);
+	data[0]=0x22; //command
+	data[1]=0x36;
+	HAL_I2C_Master_Transmit(&hi2c1,thermal_address<<1,data,2,50);
+	HAL_Delay(50);
+	HAL_I2C_Master_Receive(&hi2c1,thermal_address<<1,readbuff,6,50);
+	 temp = (readbuff[0] * 256) + readbuff[1];
+	 cTemp = -45.0 + (175.0 * temp / 65535.0);
+	 humi = (readbuff[3] * 256) + readbuff[4];
+	 cHumi = (100.0 * humi / 65535.0);
+	 i=abs(25-cTemp);
+
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(GPIO_Pin);
+  if(GPIO_Pin==GPIO_PIN_13)
+	{
+       if(workflag==1)
+       {
+		  pir();
+       }
+	}
+}
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(htim);
+  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_1,GPIO_PIN_SET);
+
+}
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC|RCC_PERIPHCLK_ADC;
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+  /** Common config 
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel 
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief RTC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RTC_Init(void)
+{
+
+  /* USER CODE BEGIN RTC_Init 0 */
+
+  /* USER CODE END RTC_Init 0 */
+
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef DateToUpdate = {0};
+
+  /* USER CODE BEGIN RTC_Init 1 */
+
+  /* USER CODE END RTC_Init 1 */
+  /** Initialize RTC Only 
+  */
+  hrtc.Instance = RTC;
+  hrtc.Init.AsynchPrediv = RTC_AUTO_1_SECOND;
+  hrtc.Init.OutPut = RTC_OUTPUTSOURCE_ALARM;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN Check_RTC_BKUP */
+    
+  /* USER CODE END Check_RTC_BKUP */
+
+  /** Initialize RTC and set the Time and Date 
+  */
+  sTime.Hours = 0x16;
+  sTime.Minutes = 0x18;
+  sTime.Seconds = 0x0;
+
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  DateToUpdate.WeekDay = RTC_WEEKDAY_SATURDAY;
+  DateToUpdate.Month = RTC_MONTH_MARCH;
+  DateToUpdate.Date = 0x1;
+  DateToUpdate.Year = 0x20;
+
+  if (HAL_RTC_SetDate(&hrtc, &DateToUpdate, RTC_FORMAT_BCD) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RTC_Init 2 */
+
+  /* USER CODE END RTC_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 40000;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 60000;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA1 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB3 PB4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
